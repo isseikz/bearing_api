7e690f98bf96209646acf37ade8f28111c8b22ca
isseik <issey.macky@gmail.com>
2017-11-10 01:31:19 +0000

    scaffold database

---
+++ db/migrate/20171110012704_create_locations.rb
@@ -0,0 +1,10 @@
+class CreateLocations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :locations do |t|
+      t.float :longitude
+      t.float :lattitude
+
+      t.timestamps
+    end
+  end
+end
